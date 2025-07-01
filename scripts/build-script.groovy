1bb27eb8f0465c354e1e6898bb8933ea66210305
Ranya Azizi Kadmiri <101256609+ranyakadmiri@users.noreply.github.com>
2025-07-01 16:17:27 +0100

    test

--- scripts/build-script.groovy
+++ scripts/build-script.groovy
@@ -26,8 +26,8 @@ try {
     //remove existing container 
     runCommand('docker rm -f temp-extract || true', BACKEND_DIR)
     runCommand('docker create --name temp-extract nomprenomlasseexamen-backend', BACKEND_DIR)
-    runCommand('docker cp temp-extract:/app/target/. ./target/', BACKEND_DIR)
-    runCommand('docker rm temp-extract', BACKEND_DIR)
+    //runCommand('docker cp temp-extract:/app/target/. ./target/', BACKEND_DIR)
+   // runCommand('docker rm temp-extract', BACKEND_DIR)
     
     println "Build completed successfully!"
     println "Backend JAR: ${BACKEND_DIR}/target/*.jar"
